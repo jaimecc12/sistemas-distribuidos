24801478909ac71747e9bf1ec4b581bd48b7ab4d
Jaimecc12 <jaimecc0313@gmail.com>
2021-11-14 09:53:03 -0600

    login

---
+++ views/login/login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>este es un login mamalon</title>
+    <link rel="stylesheet" href="../../resources/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../../resources/css/estilo.css">
+</head>
+<body>
+    <h1>hola este es el login we</h1>
+
+
+
+    <section class="vh-100" >
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100" style="background-color: #e8e8e8">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <img src="https://cdn-icons-png.flaticon.com/512/2026/2026508.png" class="atro img-fluid" alt="Phone image">
+      </div>
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <form>
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="email" id="form1Example13" class="form-control form-control-lg" />
+            <label class="form-label" for="form1Example13">Email address</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-4">
+            <input type="password" id="form1Example23" class="form-control form-control-lg" />
+            <label class="form-label" for="form1Example23">Password</label>
+          </div>
+
+          <div class="d-flex justify-content-around align-items-center mb-4">
+            <!-- Checkbox -->
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="form1Example3"
+                checked
+              />
+              <label class="form-check-label" for="form1Example3"> Remember me </label>
+            </div>
+            <a href="#!">Forgot password?</a>
+          </div>
+
+          <!-- Submit button -->
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+
+          
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
